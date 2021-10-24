3cc621f6d3e02acf5cd1c504b502343a3a43c23c
warmich <75064346+warmich@users.noreply.github.com>
2021-10-24 14:04:36 +0200

    component skill

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevComponent } from './components/dev/dev.component';
 import { SkillsComponent } from './components/skills/skills.component';
 
 const routes: Routes = [
-  {path: 'skills', component: SkillsComponent}
+  {path: 'skills', component: SkillsComponent},
+  {path: 'dev', component: DevComponent}
 ];
 
 @NgModule({
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SKILLS } from './listSkills';
+import { Skill } from './skill';
 
 @Component({
   selector: 'app-skills',
